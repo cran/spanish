df7f677cdfa972c32d26dbabebc39a7e4d9a2b0d
Jose Manuel Vera Oteo <vera.josemanuel@gmail.com>
2018-01-08 12:57:21 +0000

    version 0.3.2

---
+++ R/spanish.R
@@ -0,0 +1,16 @@
+#' spanish: A package for spanish related data functions.
+#'
+#' The spanish package provides two important functions:
+#' to_number() and geocode_cadastral()
+#'
+#' @section to_number():
+#' Translate spanish spelled quantities into their integer counterparts.
+#' Allows you to translate to integer numerical words spelled in spanish.
+#' Text must be previously cleaned & removed extraneous words or symbols.
+#' Quantities MUST be written in a correct Spanish (this is not a grammar tool)
+#' The upper limit is up to the millions range. Cents must be removed.
+#' (in my TODO list to parse cents part)
+#'
+#' @docType package
+#' @name spanish
+NULL
